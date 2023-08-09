3be2c9c0491b91140bfd3129a7842fa3dbb365c9
Sergio Diez <diezgarciasergioo@gmail.com>
2023-08-09 17:49:52 +0200

    salen cartas

--- src/main.ts
+++ src/main.ts
@@ -1,91 +1,97 @@
+const MAXIMO_PUNTUACION: number = 7;
+let puntuacion: number = 0;
 
-const generarNumeroAleatorio = (): number => Math.floor(Math.random() * 101)   
-
-const numeroparaAcertar: number = generarNumeroAleatorio();
-
-type Estado =
- | "NO_ES_UN_NUMERO"
- | "EL_NUMERO_ES_MAYOR"
- | "EL_NUMERO_ES_MENOR"
- | "ES_EL_NUMERO_SECRETO"
- | "GAME_OVER_MAXIMO_INTENTOS";
-
- const MAXIMO_INTENTOS: number = 5;
-let numeroDeIntentos: number = 0;
-
-const hasSuperadoElNumeroMaximoDeIntentos = () =>
- numeroDeIntentos >= MAXIMO_INTENTOS;
-
- const muestraNumeroDeIntentos = () => {
-    const elementoIntentos = document.getElementById("intentos");
-    if (elementoIntentos) {
-    elementoIntentos.innerHTML = `${numeroDeIntentos} de ${MAXIMO_INTENTOS}`; 
-    }
- }
-
- document.addEventListener("DOMContentLoaded", muestraNumeroDeIntentos);
-    
- 
-
- const muestraMensajeComprobacion = (texto : string, estado : Estado ) => {
-    let mensaje : string = "";
-    switch ( estado) {
-        case "NO_ES_UN_NUMERO" :
-            mensaje = `${texto} no es un numero😔`
-            break;
-        case "EL_NUMERO_ES_MAYOR" :
-            mensaje = `${texto} es menor que el numero😔`
-            break;
-            case "EL_NUMERO_ES_MENOR" :
-            mensaje = `${texto} es mayor que el numero😔`
-            break;
-        case "ES_EL_NUMERO_SECRETO" :
-            mensaje = `${texto} es el numero secreto, bien hecho!!!💯💯`
-            break;
-        case "GAME_OVER_MAXIMO_INTENTOS" :
-            mensaje = `GAME OVER 👾👾`
-            break;
-        default :
-        mensaje = `que coño has hecho`
-
-            
-    } 
-    const elementoResultado = document.getElementById("resultado")
-    if(elementoResultado) {
-        elementoResultado.innerHTML = mensaje
-    }else{
-        console.error("muestraMensajeComprobacion: No se ha encontrado el elemento con id resultado"); 
-    }
-    
+type Estado = {
+  UNO_COPAS: 1;
+  DOS_COPAS: 2;
+  TRES_COPAS: 3;
+  CUATRO_COPAS: 4;
+  CINCO_COPAS: 5;
+  SEIS_COPAS: 6;
+  SIETE_COPAS: 7;
+  SOTA_DE_COPAS: 0.5;
+  CABALLO_DE_COPAS: 0.5;
+  REY_DE_COPAS: 0.5;
+};
+const muestraNumeroPuntuacion = () => {
+  const elementoPuntuacion = document.getElementById("puntuacion");
+  if (elementoPuntuacion) {
+    elementoPuntuacion.innerHTML = `${puntuacion} de ${MAXIMO_PUNTUACION}`;
+  }
 };
 
-const comprobarNumero = (texto : string) : Estado => {
-    const numero = parseInt(texto);
-    const esUnNumero = !isNaN(numero);
-    if (!esUnNumero) {
-    return "NO_ES_UN_NUMERO";
-    }
-    if (numero === numeroparaAcertar) {
-    return "ES_EL_NUMERO_SECRETO";
-    }
-    if (hasSuperadoElNumeroMaximoDeIntentos()) {
-    return "GAME_OVER_MAXIMO_INTENTOS";
-    }
-    return numero > numeroparaAcertar ? "EL_NUMERO_ES_MAYOR" : "EL_NUMERO_ES_MENOR";
-   };
+document.addEventListener("DOMContentLoaded", muestraNumeroPuntuacion);
 
-   const handleCompruebaClick = () => {
-    const texto = document.getElementById("numero");
-    if (texto) {
-        texto.innerHTML = "mensaje" ;
-    }else{
-     console.error("muestraMensajeComprobacion: No se ha encontrado el elemento con id resultado");   
-    }
-    const estado = comprobarNumero("texto");
-    muestraMensajeComprobacion("texto", estado);
-    numeroDeIntentos++;
-    muestraNumeroDeIntentos();
-    gestionarGameOver(estado);
- };
-   
+// Implementa una función que te de un número aleatorio entre 1 y 12
+// Cuando tengas esto, mira como lo refinarías para generar un numero que sea:
+// 1,2,3,4,5,6,7,10,11,12
+const dameCarta = (): number => {
+  let nuevaCarta = 0;
+  // Loq que sea con random
+  nuevaCarta = Math.floor(Math.random() * 10) + 1;
 
+  // Si nuevaCarta > 7
+  // nuevaCarta = nuevaCarta + 2;
+  if (nuevaCarta > 7) {
+    nuevaCarta = nuevaCarta + 2;
+  }
+
+  return nuevaCarta;
+};
+
+let nuevaCarta = dameCarta();
+console.log(nuevaCarta);
+
+// img
+// src
+//./content/back.jpg
+const muestraCarta = (numerocarta: number) => {
+  let urlImagen = "./content/back.jpg";
+  switch (numerocarta) {
+    case 1:
+      urlImagen = "./content/1-copas.jpg";
+      break;
+    case 2:
+      urlImagen = "./content/2-copas.jpg";
+      break;
+    case 3:
+      urlImagen = "./content/3-copas.jpg";
+      break;
+    case 4:
+      urlImagen = "./content/4-copas.jpg";
+      break;
+    case 5:
+      urlImagen = "./content/5-copas.jpg";
+      break;
+    case 6:
+      urlImagen = "./content/6-copas.jpg";
+      break;
+    case 7:
+      urlImagen = "./content/7-copas.jpg";
+      break;
+    case 10:
+      urlImagen = "./content/10-sota-copas.jpg";
+      break;
+    case 11:
+      urlImagen = "./content/11-caballo-copas.jpg";
+      break;
+    case 12:
+      urlImagen = "./content/12-rey-copas.jpg";
+      break;
+    // 10,11,12
+  }
+
+  const cartaElemento = document.getElementById("carta");
+  if (cartaElemento instanceof HTMLImageElement) {
+    cartaElemento.src = urlImagen;
+  }
+};
+
+const botonDameCarta = document.getElementById("dameCarta");
+
+if (botonDameCarta instanceof HTMLButtonElement) {
+  botonDameCarta.addEventListener("click", () => {
+    const nuevoNumero = dameCarta();
+    muestraCarta(nuevoNumero);
+  });
+}
